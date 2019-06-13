c5e5991e4cc75be407a6c83de5fb40534ba61e83
Adella <adellakenn@gmail.com>
2019-06-13 19:14:32 +0000

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,17 @@
+=begin
+  Student
+  #hello
+    returns a greeting phrase (FAILED - 3)
+  #raise_hand
+    returns the phrase 'Pick me!' (FAILED - 4)
+=end
+
+class Student
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+  
+  def raise_hand
+    puts "Pick me!"
+  end
+end
